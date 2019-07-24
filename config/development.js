6250ef095c13cbda40084dae333f3800ebc1c158
Danial Minassian <daniel.minass@gmail.com>
2019-07-24 16:02:41 +0200

    Unused imports

--- config/development.js
+++ config/development.js
@@ -3,8 +3,12 @@ const config = {
   hostname: 'localhost',
   port: 3000,
   db: {
-    url: 'localhost',
-    db_name:'eze'
+    // url: 'localhost',
+    // db_name:'eze'
+    url: 'ezecluster-0ouhd.mongodb.net',
+    username: 'admin_eze',
+    password: 'wDrrdsidWuSCgKcZ',
+    db_name:'business'
   },
   mapbox: {
     endpoint: 'mapbox.places',
